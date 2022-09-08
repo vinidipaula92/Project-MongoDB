370caff4fd8b40ab85574d793c1cb2b76d313ce2
vinidipaula92 <vinidipaula92@gmail.com>
2022-09-08 18:12:17 -0300

    :bug: fix: Criação de collection estava incompleta, agora fazendo a contagem dos produtos

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,3 +1,3 @@
 db.createCollection("resumoProdutos");
-db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos: 5 });
+db.resumoProdutos.insertOne({ franquia: "McDonalds", totalProdutos: db.produtos.count({}) });
 db.resumoProdutos.find({}, { franquia: 1, totalProdutos: 1, _id: 0 });
